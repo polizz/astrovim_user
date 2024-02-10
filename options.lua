9324a558e7fbf2ad6ea061e014dcd912129fef05
polizz <polizz@nowhere>
2024-02-10 13:09:34 -0600

    bump ttimeoutlen

--- options.lua
+++ options.lua
@@ -2,7 +2,7 @@
 return {
   opt = {
     -- set to true or false etc.
-    timeoutlen = 100,
+    timeoutlen = 75,
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
